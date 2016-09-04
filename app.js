643bd1705d49f0173f739da4cfa9669177df0d58
llibicpep <llibicpep@mail.ru>
2016-09-04 15:09:30 +0300

    Allow Authorization header

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(cookieParser());
  */
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Authorization");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     return next();
 });
